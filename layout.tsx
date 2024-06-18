f55f7d418abea570644247a78150278e2247c289
vikasKumar889 <104764228+vikasKumar889@users.noreply.github.com>
2024-06-19 00:40:45 +0530

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import "react-datepicker/dist/react-datepicker.css";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SyncSpace",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/ss2.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <html lang="en">
+      <ClerkProvider
+        appearance={{
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/icons/ss.png",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#86469C",
+            colorInputBackground: "#9D84B7",
+            colorInputText: "#fff",
+          },
+        }}
+      >
+        <body className={`${inter.className} bg-dark-2`}>
+          <Toaster />
+          {children}
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}
